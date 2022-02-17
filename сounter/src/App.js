44666d34cee75088cf869a2008070993946d231a
KimK8 <xxlbomb@gmail.com>
2022-02-17 13:07:04 +0200

    counter

---
+++ сounter/src/App.js
@@ -0,0 +1,21 @@
+//import logo from './logo.svg';
+import React, { useState } from 'react'; 
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      {count <= 10
+        ? <p>You have pressed the button {count} times </p>
+        : <p>You have pressed the button more than 10 times </p>
+      }
+    <button onClick={() => setCount(count + 1)}>+</button>
+    <button onClick={() => {if (count > 0) setCount(count - 1)}}>-</button>
+    <button onClick={() => setCount(0)}>Clear</button>
+  </div>
+);
+}
+
+export default App;
